dfb33022e8fe2ee32afd75e14d0791d2c5b348e7
OneUp5hroom <github@alcanweb.com>
2019-08-31 09:13:51 -0500

    2

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,14 +8,41 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Scott's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            Console.WriteLine("Please enter Grades for Scott, Q to quit");
+            while(true)
+            {
+                var input = Console.ReadLine();
+                if (input == "q" || input == "Q")
+                {
+                    break;
+                }
+                var grade = 0.0;
+                if (double.TryParse(input, out grade)) {
+                    try{
+                        book.AddGrade(grade);
+                    }
+                    catch(ArgumentException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        Console.WriteLine("**");
+                    }
+                    
+                } 
+                else 
+                {
+                    Console.WriteLine($"What the heck was that crap? you think {input} looks like a grade?");
+                }
+            };
+
             var stats = book.GetStatistics();
 
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
     }
 }
